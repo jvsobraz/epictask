94867c07a79a5c89db60cf8dce37b18287b31241
jvsobraz <jvsobraz@hotmail.com>
2023-10-03 10:07:33 -0300

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TASK (title, description, score, status) VALUES ('Prototipo', 'Criar o prototipo no figma', 50, 30);
+INSERT INTO TASK (title, description, score) VALUES ('BD', 'Modelar o banco de dados do webapp', 40);
+INSERT INTO TASK (title, description, score) VALUES ('Teste', 'Criar os testes unitarios', 60);
